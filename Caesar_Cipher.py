17236e03dc0c809cf76458cd0260ac5bf7a5ee58
abhowcpt025 <abhowcpt025@student.wethinkcode.co.za>
2026-06-17 16:14:34 +0200

    Caeser Cipher ready

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,19 +1,19 @@
 # --- Caesar Cipher ---
 
-def caesar_cipher(message, shift, mode):
+def caesar_cipher(text, shift, mode):
     
     result = ""
 
     if mode == "decrypt":
         shift = -shift
 
-    for char in message:
+    for char in text:
         
         if char.isalpha():
-            # if char.isupper():
-            #     new_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
-            # else:
-            #     new_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            if char.isupper():
+                new_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                new_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
 
             result += new_char
             
@@ -21,3 +21,32 @@ def caesar_cipher(message, shift, mode):
             result += char
 
     return result
+
+print("--- Caeser Cipher ---")
+
+while True:
+    choice = input("Encrypt or Decrypt? (e/d):").lower()
+
+    if choice in ["e", "d"]:
+        break
+
+    print("Enter 'e' or 'd' only")
+
+message = input("Enter your encrypted message:")
+
+while True:
+    try:
+        shift = int(input("Enter shift value:"))
+        break
+    except ValueError:
+        print("Enter a number")
+
+if choice == "e" :
+    output = caesar_cipher(message, shift, "encrypt")
+    print("\nEncrypted message:")
+
+else:
+    output = caesar_cipher(message, shift, "decrypt")
+    print("\nDecrypted message:")
+
+print(output)
